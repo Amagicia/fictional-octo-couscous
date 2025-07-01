a52c408517605911a20e407ed98d0dc9ba521756
Aman Tiwari <amntwri0323@gmail.com>
2025-07-01 23:46:26 +0530

    done app folder

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@db-host:5432/yourdbname"
+DATABASE_URL = "postgresql://location_1698_user:Scmgt1Keu8Y4SgFsoTM0OVG6PKAUg1Hu@dpg-d1fbfsfgi27c73ckorkg-a/location_1698"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
